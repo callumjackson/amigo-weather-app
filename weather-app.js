7c5ac5660d493f593dfeb0222aa01308a888fdec
Callum Jackson <j1288048@student.southstaffs.ac.uk>
2019-11-26 20:00:30 +0000

    added a footer and copyright
    added my name to the site as well as the copyright logo.
    
    as well as added comments to the code to clearly lay out the code and what each are is/ does

--- weather-app.js
+++ weather-app.js
@@ -7,12 +7,12 @@ window.addEventListener('load', ()=> {
   let degreeSection = document.querySelector('.degree-section');
   const tempSpan = document.querySelector('.degree-section span')
 
-
+// this if command looks for the users coords once they have given permission
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
-      const proxy = "https://cors-anywhere.herokuapp.com/"; //proxy is used because of having to work on a local host.
+      const proxy = "https://cors-anywhere.herokuapp.com/"; //proxy is used because of having to work thorugh a local host.
       const api = `${proxy}https://api.darksky.net/forecast/efe32efe404ba1b34b71bb4aa98b1a87/${lat},${long}`;
 
       fetch(api)
@@ -21,14 +21,14 @@ window.addEventListener('load', ()=> {
       })
       .then(data => {
         console.log(data);
-        const {temperature, summary, icon} = data.currently
+        const {temperature, summary, icon} = data.currently //this reads the data from the API so that the website can use it
 
         tempDegree.textContent = temperature;
         tempDesc.textContent = summary;
         locChoice.textContent = data.timezone;
 
         //forular to covert to celsius
-        let celsius = (temperature - 32) * (5 / 9)
+        let celsius = (temperature - 32) * (5 / 9) //this is the formular that converts the temperatures
 
 
         // set icon to the current weather.
@@ -53,7 +53,7 @@ window.addEventListener('load', ()=> {
   }
    function setIcons(icon, iconID){
      const skycons = new Skycons({color: "white"});
-     const currentIcon = icon.replace(/-/g, "_").toUpperCase(); // this removes the - in the skycon file and replaces it with _ as well as makes it everything uppercase to match the api
+     const currentIcon = icon.replace(/-/g, "_").toUpperCase(); // This removes the - in the skycon file and replaces it with _ as well as makes it everything uppercase to match the api
      skycons.play();
      return skycons.set(iconID, Skycons[currentIcon]);
    }
