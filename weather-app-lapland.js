8909d71bee654268b98000b4dc0c8faa45e9f253
Callum Jackson <j1288048@student.southstaffs.ac.uk>
2019-11-26 19:41:05 +0000

    finished product (before testing)
    this is the final product, once testing and tweeking has been completed, the project will be completed

--- weather-app-lapland.js
+++ weather-app-lapland.js
@@ -3,7 +3,6 @@ window.addEventListener('load', ()=> {
   let lat;
   let tempDesc = document.querySelector('.temp-description');
   let tempDegree = document.querySelector('.temp-degree');
-  let locChoice = document.querySelector('.location-choice');
   let degreeSection = document.querySelector('.degree-section');
   const tempSpan = document.querySelector('.degree-section span')
 
@@ -13,7 +12,7 @@ window.addEventListener('load', ()=> {
       long = position.coords.longitude;
       lat = position.coords.latitude;
       const proxy = "https://cors-anywhere.herokuapp.com/"; //proxy is used because of having to work on a local host.
-      const api = `${proxy}https://api.darksky.net/forecast/efe32efe404ba1b34b71bb4aa98b1a87/${lat},${long}`;
+      const api = `${proxy}https://api.darksky.net/forecast/efe32efe404ba1b34b71bb4aa98b1a87/90.0000,135.0000`;
 
       fetch(api)
       .then(response =>{
@@ -25,7 +24,6 @@ window.addEventListener('load', ()=> {
 
         tempDegree.textContent = temperature;
         tempDesc.textContent = summary;
-        locChoice.textContent = data.timezone;
 
         //forular to covert to celsius
         let celsius = (temperature - 32) * (5 / 9)
